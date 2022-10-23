e69623edc84cd4d50beaa774509d8dda4b7ca501
BenLiu <liumanpan1990@hotmail.com>
2022-10-23 16:59:13 -0400

    complete category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,76 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const dataList = await Category.findAll({
+    include: Product,
+  });
+  res.json(dataList);
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const data = await Category.findByPk(req.params.id, {
+      include: Product,
+    });
+    data ? res.json(data) : res.status(404).json("No such ID");
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const affected = await Category.create(req.body);
+    res.status(200).json(affected.get());
+  } catch (error) {
+    res.status(404).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const affected = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affected[0]) {
+      res.status(200).json(`update successfully`);
+    } else {
+      res.status(404).json("No such category id / invalid input");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const affected = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affected) {
+      res.status(200).json(`deleted ${affected} successfully`);
+    } else {
+      res.status(404).json("No such category id / invalid input");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
