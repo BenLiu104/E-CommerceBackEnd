7b6dfdfe23021ecceb39fdf330a4b9c3c1173d90
BenLiu <liumanpan1990@hotmail.com>
2022-10-23 16:58:38 -0400

    complete tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,76 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  const dataList = await Tag.findAll({
+    include: [{ model: Product, through: ProductTag }],
+  });
+  res.json(dataList);
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const data = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag }],
+    });
+    data ? res.json(data) : res.status(404).json("No such ID");
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const result = await Tag.create(req.body);
+    res.status(200).json(result.get());
+  } catch (error) {
+    res.status(404).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const affected = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affected[0]) {
+      res.status(200).json(`update successfully`);
+    } else {
+      res.status(404).json("No such category id / invalid input");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const affected = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (affected) {
+      res.status(200).json(`deleted ${affected} successfully`);
+    } else {
+      res.status(404).json("No such category id / invalid input");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
